69b52006de898625367df97d859a3e6e97ca5392
lorenzonieto <150736624+lorenzonieto@users.noreply.github.com>
2025-10-17 01:57:54 -0400

    Update go.py
    add comments and reorganize some code for the sake of readability

--- go.py
+++ go.py
@@ -5,13 +5,13 @@ parser = argparse.ArgumentParser(description="Classic strategy game Go implement
 parser.add_argument("-s", "--size", type=int, default=19, help="length/width of the board (default is 19x19)")
 parser.add_argument("-b", "--bonus", type=float, default=0, help="the amount of bonus points added to white's score (default is 0)")
 args = parser.parse_args()
-
+# initialize pygame
 pygame.init()
 clock = pygame.time.Clock()
 pygame.display.set_caption("Go")
 fpsLimit = 60
 running = True
-
+# initialize screen/board dimensions
 screen_size = pygame.display.get_desktop_sizes()[0][1] * 3 / 4
 screen = pygame.display.set_mode((screen_size, screen_size))
 board_size = args.size
@@ -19,9 +19,10 @@ tile_size = screen_size / (board_size + 1)
 stone_radius = tile_size / 2
 tengen_size = max(tile_size / 8, 2)
 ghost_thickness = max(int(tile_size / 15), 2)
-board = [[-1 for i in range(board_size)] for j in range(board_size)]
 board_pos = [tile_size * i for i in range(1, board_size + 1)]
 center_coord = board_pos[board_size // 2]
+# initialize game state
+board = [[-1 for i in range(board_size)] for j in range(board_size)]
 colors = ("black", "white")
 turn = False
 click_release = True
@@ -29,17 +30,18 @@ pass_release = True
 stone_list = [set(), set()]
 board_history = {}
 pass_count = 0
-
 ghost_pos = [0, 0]
-
+# draws the game board and pieces
 def show_board():
     screen.fill((240, 170, 100))
     # draw board lines
     for i in range(board_size):
         pygame.draw.line(screen, "black", (board_pos[i], board_pos[0]), (board_pos[i], board_pos[board_size - 1]), 2)
         pygame.draw.line(screen, "black", (board_pos[0], board_pos[i]), (board_pos[board_size - 1], board_pos[i]), 2)
+    # draw tengen if board dimensions are odd
     if board_size % 2:
         pygame.draw.circle(screen, "black", (center_coord, center_coord), tengen_size)
+    # draw side and corner points if board is 19x19
     if board_size == 19:
         for i in range(3):
             for j in range(3):
@@ -56,15 +58,15 @@ def show_board():
 
 def clamp(n, floor, ceiling):
     return min(max(n, floor), ceiling)
-
+# get position of ghost in board coordinates
 def get_ghost(raw_x, raw_y):
     mouse_x = clamp(((raw_x + stone_radius) // tile_size), 1, board_size) - 1
     mouse_y = clamp(((raw_y + stone_radius) // tile_size), 1, board_size) - 1
     return [int(mouse_x), int(mouse_y)]
-
+# draw ghost at point (x, y)
 def show_ghost(x, y):
     pygame.draw.circle(screen, colors[turn], (board_pos[x], board_pos[y]), stone_radius, ghost_thickness)
-
+# get the neighbors of a given point
 def neighborhood(point):
     neighbors = []
     if point[0] > 0:
@@ -76,7 +78,7 @@ def neighborhood(point):
     if point[1] < (board_size - 1):
         neighbors.append((point[0], point[1] + 1))
     return neighbors
-
+# get the set of points of a given point's group, along with the group's types of neighbors
 def get_group(point):
     group = set()
     unexplored = {point}
@@ -98,30 +100,34 @@ def get_group(point):
 def debug():
     #print(get_group(tuple(ghost_pos)))
     return
-
-def do_move(point, turn, ):
+# update the board's state based on the given turn and move point, and return the next turn
+def do_move(point, turn,):
     opponent = not turn
     if not(board[point[0]][point[1]] == -1):
         return turn
     board[point[0]][point[1]] = turn
     checked = set()
     killed_by_move = set()
+    # get all stones that would be killed on this move
     for neighbor in neighborhood(point):
         if board[neighbor[0]][neighbor[1]] == opponent and neighbor not in checked:
             group, alive = get_group(neighbor)
             checked.update(group)
             if not alive[2]:
                 killed_by_move.update(group)
+    # prevent suicide
     if not killed_by_move:
         self_group, self_alive = get_group(point)
         if not self_alive[2]:
             board[point[0]][point[1]] = -1
             return turn
+    # update list of stones that would result after move
     new_stone_list = [stone_list[0].copy(), stone_list[1].copy()]
     new_stone_list[turn].add(point)
     new_stone_list[opponent] -= killed_by_move
     new_stone_count = len(new_stone_list[0]) + len(new_stone_list[1])
     history = board_history.get(new_stone_count)
+    # check if board state has occured previously in the game; if so, reject it
     if not history:
         board_history.update({new_stone_count: [new_stone_list]})
     else:
@@ -130,13 +136,14 @@ def do_move(point, turn, ):
                 board[point[0]][point[1]] = -1
                 return turn
         history.append(new_stone_list)
+    # update stone list and board state
     stone_list.clear()
     stone_list.append(new_stone_list[0])
     stone_list.append(new_stone_list[1])
     for death in killed_by_move:
         board[death[0]][death[1]] = -1
     return not turn
-
+# determine the score of the game and print result
 def score_game():
     if args.bonus.is_integer():
         score = [0, int(args.bonus)]
@@ -160,22 +167,22 @@ def score_game():
                     scored.update(group)
     print("Black " + str(score[0]) + "-" + str(score[1]) + " White")
     return False
-
+# call drawing functions
 def display_frame():
     show_board()
     show_ghost(ghost_pos[0], ghost_pos[1])
-
+# main game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    # poll for mouse input to get ghost location
     ghost_pos = get_ghost(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
 
     display_frame()
 
     clicks = pygame.mouse.get_pressed()
-
+    # player has attempted to place a stone, move must be handled
     if clicks[0] and click_release:
         click_release = False
         turn = do_move(tuple(ghost_pos), turn)
@@ -184,7 +191,7 @@ while running:
         click_release = True
 
     keys = pygame.key.get_pressed()
-
+    # player has passed their turn
     if keys[pygame.K_SPACE] and pass_release:
         pass_release = False
         pass_count += 1
@@ -193,7 +200,7 @@ while running:
         turn = not turn
     if not keys[pygame.K_SPACE]:
         pass_release = True
-
+    # player has pressed debug button
     if keys[pygame.K_d]:
         debug()
 
